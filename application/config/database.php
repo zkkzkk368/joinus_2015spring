c98a905f69be2d710ac1bf476318b6bf8510c84a
zkkzkk368 <3140100805@zju.edu.cn>
2015-03-21 11:55:47 +0800

    joinus_2015spring

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'q92583582880,';
+$db['default']['password'] = '123456';
 $db['default']['database'] = 'join';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
